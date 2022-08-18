93b2edf728698ca14a6253ade533f577b7b4d98a
Shivansh Goel <shivanshgoel21@gmail.com>
2022-08-18 23:35:04 +0530

    crm complete

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mysql = require("mysql");
 const logger = require("./logger/logger");
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
-
-//#region db connection setup
-var isDbConnected = true;
-// const mysqlConnection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'shivansh'
-// });
-
-// mysqlConnection.connect((err)=> {
-//   if(!err) {
-//     isDbConnected = true;
-//     console.log('Connection Established Successfully');
-//   }
-//   else {
-//     console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
-//   }
-// });
-//#endregion db connection setup
 
 // global middlewares
 app.use(cors());
@@ -31,17 +11,10 @@ app.use(express.json());
 
 // logs are for dev only
 app.use((req, res, next) => {
-  if (!isDbConnected) {
-    logger.log(`DB NOT CONEECTED :- REJECTED INCOMING REQUEST AT ROUTE: ${req.path}`, 0);
-    res.json({
-      message: "error",
-      detail: "Service not available for accepting requests",
-    });
-  } else{
     logger.log(`PROCESSING INCOMING REQUEST AT ROUTE: ${req.path}`, 0);
     next();
   }
-});
+);
 
 // app routes
 app.use("/contact", require("./routes/contactRoutes"));
